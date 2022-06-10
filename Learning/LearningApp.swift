4f1b3b0b4cb4160582332241b78aa0a0ef5f463e
lirtual <lirtual.gmail.com>
2022-06-10 17:45:04 +0800

    Initial Commit

---
+++ Learning/LearningApp.swift
@@ -0,0 +1,17 @@
+//
+//  LearningApp.swift
+//  Learning
+//
+//  Created by Jianhui Ye on 2022/6/10.
+//
+
+import SwiftUI
+
+@main
+struct LearningApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
